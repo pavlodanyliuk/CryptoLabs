194594e5474f4ed38eef2168eba8bb8059ca275a
roland <pavlodanyliuk@gmail.com>
2018-03-17 18:39:36 +0200

    Created project

---
+++ Lab_1/src/net/cryptodanyliuk/main/Main.java
@@ -0,0 +1,10 @@
+package net.cryptodanyliuk.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ Lab_1/src/net/cryptodanyliuk/utils/FileCleaner.java
@@ -0,0 +1,6 @@
+package net.cryptodanyliuk.utils;
+
+public class FileCleaner {
+
+    
+}
